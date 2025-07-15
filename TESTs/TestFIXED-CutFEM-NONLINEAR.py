480b7afba9d92b6d37559f1b7eecf0d08f10715d
Elmanyer <pau.manyer@hotmail.com>
2025-07-15 16:25:33 +0200

    Add .py fixed tests #main

---
+++ TESTs/TestFIXED-CutFEM-NONLINEAR.py
@@ -0,0 +1,136 @@
+import sys
+sys.path.append('../src/')
+
+from GradShafranovSolver import *
+
+############# EQUILIBRIUM PROBLEM PARAMETERS ##############
+
+## CREATE GRAD-SHAFRANOV PROBLEM
+Equilibrium = GradShafranovSolver()
+
+## DECLARE SWITCHS:
+##### GHOST PENALTY STABILISATION
+Equilibrium.FIXED_BOUNDARY = True
+Equilibrium.GhostStabilization = True
+Equilibrium.PARALLEL = False
+
+##### OUTPUT PLOTS IN RUNTIME
+Equilibrium.plotelemsClas = False      # OUTPUT SWITCH FOR ELEMENTS CLASSIFICATION PLOTS AT EACH ITERATION
+Equilibrium.plotPSI = False            # OUTPUT SWITCH FOR PSI SOLUTION PLOTS AT EACH ITERATION
+##### OUTPUT FILES
+Equilibrium.out_proparams = True       # OUTPUT SWITCH FOR SIMULATION PARAMETERS 
+Equilibrium.out_elemsClas = True       # OUTPUT SWITCH FOR CLASSIFICATION OF MESH ELEMENTS
+Equilibrium.out_plasmaLS = True        # OUTPUT SWITCH FOR PLASMA BOUNDARY LEVEL-SET FIELD VALUES
+Equilibrium.out_plasmaBC = True        # OUTPUT SWITCH FOR PLASMA BOUNDARY CONDITION VALUES 
+Equilibrium.out_plasmaapprox = True    # OUTPUT SWITCH FOR PLASMA BOUNDARY APPROXIMATION DATA 
+Equilibrium.out_ghostfaces = True      # OUTPUT SWITCH FOR GHOST STABILISATION FACES DATA 
+Equilibrium.out_elemsys = False        # OUTPUT SWITCH FOR ELEMENTAL MATRICES
+##### OUTPUT PICKLING
+Equilibrium.out_pickle = True          # OUTPUT SWITCH FOR SIMULATION DATA PYTHON PICKLE
+
+# DEFINE NUMERICAL_TREATMENT PARAMETERS  
+Equilibrium.dim = 2                    # PROBLEM SPATIAL DIMENSION
+Equilibrium.QuadratureOrder2D = 8      # ORDER OF SURFACE NUMERICAL INTEGRATION QUADRATURES 
+Equilibrium.QuadratureOrder1D = 5      # ORDER OF LENGTH NUMERICAL INTEGRATION QUADRATURES 
+Equilibrium.ext_maxiter = 5            # EXTERNAL LOOP (PHI_B) MAXIMUM ITERATIONS
+Equilibrium.ext_tol = 1.0e-3           # EXTERNAL LOOP (PHI_B) CONVERGENCE TOLERANCE
+Equilibrium.int_maxiter = 10           # INTERNAL LOOP (PHI_NORM) MAXIMUM ITERATIONS
+Equilibrium.int_tol = 1.0e-4           # INTERNAL LOOP (PHI_NORM) CONVERGENCE TOLERANCE
+Equilibrium.tol_saddle = 0.1           # TOLERANCE FOR DISTANCE BETWEEN CONSECUTIVE ITERATION SADDLE POINTS (LETS PLASMA REGION CHANGE)
+Equilibrium.beta = 1.0e6               # NITSCHE'S METHOD PENALTY PARAMETER
+Equilibrium.Nconstrainedges = -1       # NUMBER OF PLAMA BOUNDARY APPROXIMATION EDGES ON WHICH CONSTRAIN BC
+Equilibrium.zeta = 1.0e-2              # GHOST PENALTY PARAMETER 
+Equilibrium.R0_axis = 6.0              # MAGNETIC AXIS OPTIMIZATION ROUTINE INITIAL GUESS R COORDINATE
+Equilibrium.Z0_axis = 1.0              # MAGNETIC AXIS OPTIMIZATION ROUTINE INITIAL GUESS Z COORDINATE
+Equilibrium.R0_saddle = 5.0            # ACTIVE SADDLE POINT OPTIMIZATION ROUTINE INITIAL GUESS R COORDINATE
+Equilibrium.Z0_saddle = -3.5           # ACTIVE SADDLE POINT OPTIMIZATION ROUTINE INITIAL GUESS Z COORDINATE
+Equilibrium.opti_maxiter = 50          # CRITICAL POINTS OPTIMIZATION ALGORITHM MAXIMAL ITERATIONS NUMBER
+Equilibrium.opti_tol = 1.0e-6          # CRITICAL POINTS OPTIMIZATION ALGORITHM SOLUTION TOLERANCE
+
+Equilibrium.InitialiseParameters()
+Equilibrium.InitialisePickleLists()
+
+################ COMPUTATIONAL DOMAIN MESH ###############
+
+###### LINEAR TRIANGULAR ELEMENT MESH
+#MESH = 'TRI03-COARSE-LINEAR'
+MESH = 'TRI03-MEDIUM-LINEAR'
+#MESH = 'TRI03-INTERMEDIATE-LINEAR'
+#MESH = 'TRI03-FINE-LINEAR'
+#MESH = 'TRI03-SUPERFINE-LINEAR'
+#MESH = 'TRI03-MEGAFINE-LINEAR'
+#MESH = 'TRI03-ULTRAFINE-LINEAR'
+
+#MESH = 'QUA04-COARSE-LINEAR'
+#MESH = 'QUA04-MEDIUM-LINEAR'
+#MESH = 'QUA04-INTERMEDIATE-LINEAR'
+#MESH = 'QUA04-FINE-LINEAR'
+#MESH = 'QUA04-SUPERFINE-LINEAR'
+#MESH = 'QUA04-MEGAFINE-LINEAR'
+#MESH = 'QUA04-ULTRAFINE-LINEAR'
+
+###### QUADRATIC TRIANGULAR ELEMENT MESH
+#MESH = 'TRI06-COARSE-LINEAR'
+#MESH = 'TRI06-MEDIUM-LINEAR'
+#MESH = 'TRI06-INTERMEDIATE-LINEAR'
+#MESH = 'TRI06-FINE-LINEAR'
+#MESH = 'TRI06-SUPERFINE-LINEAR'
+#MESH = 'TRI06-MEGAFINE-LINEAR'
+#MESH = 'TRI06-ULTRAFINE-LINEAR'
+
+#MESH = 'QUA09-COARSE-LINEAR'
+#MESH = 'QUA09-MEDIUM-LINEAR'
+#MESH = 'QUA09-INTERMEDIATE-LINEAR'
+#MESH = 'QUA09-FINE-LINEAR'
+#MESH = 'QUA09-SUPERFINE-LINEAR'
+#MESH = 'QUA09-MEGAFINE-LINEAR'
+#MESH = 'QUA09-ULTRAFINE-LINEAR'
+
+###### CUBIC TRIANGULAR ELEMENT MESH
+#MESH = 'TRI10-COARSE-LINEAR'
+#MESH = 'TRI10-MEDIUM-LINEAR'
+#MESH = 'TRI10-INTERMEDIATE-LINEAR'
+#MESH = 'TRI10-FINE-LINEAR'
+#MESH = 'TRI10-SUPERFINE-LINEAR'
+#MESH = 'TRI10-MEGAFINE-LINEAR'
+
+Equilibrium.MESH = Mesh(MESH)
+
+###################### TOKAMAK ###########################
+
+TOKmesh = Mesh('TRI03-MEGAFINE-LINEAR-REDUCED')
+Equilibrium.TOKAMAK = Tokamak(WALL_MESH = TOKmesh)
+
+############### INITIAL PLASMA BOUNDARY ##################
+
+Equilibrium.initialPHI = InitialPlasmaBoundary(EQUILIBRIUM = Equilibrium,   
+                                           GEOMETRY = 'LINEAR', # PREDEFINED MODEL
+                                           R0 = 6.0,            # MEAN RADIUS          
+                                           epsilon = 0.32,      # INVERSE ASPECT RATIO
+                                           kappa = 1.7,         # ELONGATION
+                                           delta = 0.33)        # TRIANGULARITY
+
+######### INITIAL GUESS FOR PLASMA MAGNETIC FLUX #########
+
+Equilibrium.initialPSI = InitialGuess(EQUILIBRIUM = Equilibrium,
+                                  PSI_GUESS = 'NONLINEAR',  # PREDEFINED MODEL
+                                  NOISE = True,             # WHITE NOISE 
+                                  R0 = 6.0,                 # MEAN RADIUS
+                                  A = 2.0)                  # NOISE AMPLITUDE 
+
+################# PLASMA CURRENT MODEL ##################
+
+Equilibrium.PlasmaCurrent = CurrentModel(EQUILIBRIUM = Equilibrium,
+                                     MODEL = 'NONLINEAR', # PREDEFINED MODEL
+                                     R0 = 6.0)            # MEAN RADIUS
+
+################# INITIALISE MESH DATA ##################
+
+Equilibrium.DomainDiscretisation(INITIALISATION=True)
+Equilibrium.InitialisePSI()
+
+################### LAUNCH SIMULATION ###################
+
+CASE = "TS-FIXED-NONLINEAR-CutFEM"
+
+Equilibrium.EQUILI(CASE)
